318e1d03260ace51d8b64c233eb15863b9fb9307
William Thorsen <william@skypilot.io>
2019-09-10 00:55:06 -0600

    ft: TypeScript can type-check incrementally, ignoring unstaged files
    Reallocated responsibilities from Lint-staged to Husky

--- husky.config.js
+++ husky.config.js
@@ -1,9 +1,46 @@
-const tasks = (array) => array.join(' && ');
+const os = require('os');
+
+const { createHash } = require('crypto');
+
+function computeHash(stringToHash) {
+  const hash = createHash('md5')
+    .update(stringToHash)
+    .digest('hex');
+  return hash.slice(0, 8);
+}
+
+const packageName = require('./package.json').name;
+const cleanPackageName = packageName
+  .replace('@', '')
+  .replace('/', '-');
+const dirNameHash = computeHash(__dirname);
+const tmpDir = `${os.tmpdir()}/${cleanPackageName}-${dirNameHash}`;
+
+/* This command stashes all staged and unstaged (including untracked) files. */
+const stashCommand = 'git stash --include-untracked --keep-index --quiet';
+
+/* These commands save the status code returned by the tasks, apply and delete the stash,
+ * and then exit with the saved status code. */
+const stashPopOnFailure = 'status=$?; git stash pop --quiet; exit $status';
+
+/* These commands restore unstaged files and patches. */
+const stashPopOnSuccess = 'git checkout stash -- .; git stash pop; git reset';
+
+/* The tasks to run before committing. */
+const preCommitTasks = [
+  // 'yarn run --silent check-types',
+  `tsc --project tsconfig.json --incremental --outDir ${tmpDir} --tsBuildInfoFile ${tmpDir}/.tsBuildInfo`,
+  'lint-staged',
+];
+
+
+const preCommit = '(' + stashCommand + ' && ' + preCommitTasks.join(' && ') + ')'
+  + ' || ( ' + stashPopOnFailure + ' )';
+const postCommit = stashPopOnSuccess;
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks([
-      'lint-staged',
-    ]),
+    'pre-commit': preCommit,
+    'post-commit': postCommit,
   },
 };
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,23 +1,5 @@
-const { createHash } = require('crypto');
-
-function computeHash(stringToHash) {
-  const hash = createHash('md5')
-    .update(stringToHash)
-    .digest('hex');
-  return hash.slice(0, 8);
-}
-
-const packageName = require('./package.json').name;
-const cleanPackageName = packageName
-  .replace('@', '')
-  .replace('/', '-');
-const dirNameHash = computeHash(__dirname);
-const tmpDir = `/tmp/${cleanPackageName}-${dirNameHash}`;
 module.exports = {
   '*.{js,ts}': [
     'eslint',
-  ],
-  '*.{ts}': [
-    `tsc --esModuleInterop --incremental --outDir ${tmpDir} --tsBuildInfoFile ${tmpDir}/.tsBuildInfo`,
   ],
 };
