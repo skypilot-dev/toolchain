4b9820f4746efe1608114c0edcb465b7f2345d2c
William Thorsen <william@skypilot.io>
2019-11-12 22:23:43 -0700

    Relocate directory wipe/create function for reusability
    - Renamed `recreateDir()` to `wipeAndCreateDir()`
    - Moved it from `initializeProject.unit.test.ts` to `wipeAndCreateDir.ts`

---
+++ src/utils/wipeAndCreateDir.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+
+
+function rmdirSyncRecursive(dir: string): void {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach((file) => {
+      const currentPath = path.join(dir, file);
+      if (fs.lstatSync(currentPath).isDirectory()) { // recurse
+        rmdirSyncRecursive(currentPath);
+      } else { // delete file
+        fs.unlinkSync(currentPath);
+      }
+    });
+    fs.rmdirSync(dir);
+  }
+}
+
+/* Given a directory, delete the directory if it exists and create it again. */
+export function wipeAndCreateDir(dir: string): void {
+  if(fs.existsSync(dir)) {
+    rmdirSyncRecursive(dir);
+  }
+  fs.mkdirSync(dir, { recursive: true });
+}
