2584923ec9295be47fe6c02d81f8842becd16162
William Thorsen <wthorsen@skypilot.dev>
2020-01-09 17:37:29 -0700

    fix: Typo in generated `compile-ts` script

--- src/utils/initializeProject.ts
+++ src/utils/initializeProject.ts
@@ -51,7 +51,7 @@ const scripts: ScriptEntry[] = [
   { key: 'all-cq-checks', value: 'yarn run typecheck && yarn run lint --quiet && yarn test' },
   { key: 'build', value: 'rm -rf lib && yarn run compile-ts' },
   { key: 'ci', value: 'yarn run all-ci-checks' },
-  { key: 'compile-ts', value: "babel ./src --out-dir ./lib --extensions .ts --ignore '**/__test__/*' --ignore '**/*.d.ts' && yarn run generate-typings"},
+  { key: 'compile-ts', value: "babel ./src --out-dir ./lib --extensions .ts --ignore '**/__tests__/*' --ignore '**/*.d.ts' && yarn run generate-typings"},
   { key: 'cq', value: 'yarn run all-cq-checks' },
   { key: 'generate-typings', value: 'tsc --project tsconfig.generate-typings.json' },
   { key: 'lint', value: "eslint --cache '**/*.{js,ts}'" },
