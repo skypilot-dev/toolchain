3bacbbc2a68b902d6c02ffdfcc79b4717e5b0b27
William Thorsen <william@thorsen.dev>
2020-02-05 09:20:50 -0700

    refactor: Rename workflow files and jobs for clarity

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 /* These configs are direct copies of the configs used in Toolchain itself. */
 export const COPIED_CONFIGS: string[] = [
-  '.github/workflows/node-ci.yml',
-  '.github/workflows/node-publish-prerelease.yml',
-  '.github/workflows/node-publish-stable.yml',
+  '.github/workflows/node-check-code.yml',
+  '.github/workflows/node-prerelease.yml',
+  '.github/workflows/node-stable-release.yml',
   '.editorconfig',
   '.eslintignore',
   '.gitignore',
